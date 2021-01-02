da71419fd473a53c965df19fd2882d27a99a4a60
flemingonasis <flemingonasis@gmail.com>
2021-01-02 14:28:49 +0700

    Finish stories proect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact' => 'signups#new'
+  post 'contact' => 'signups#create'
+  get 'thanks' => 'pages#thanks'
+  resources :signups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
